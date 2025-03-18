baf4f61c8454de9480417def61e0b221c32119cc
SungMin-hub <sungmin806@gmail.com>
2025-03-18 17:47:31 +0900

    GoMen config

---
+++ gomen/src/main/java/com/ohgiraffers/gomen/GomenApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gomen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomenApplication.class, args);
+    }
+
+}
