157292cd7ad2dd2c2313dc776668520c7eb5dc06
Hailyee <a11roundworld@naver.com>
2025-03-22 18:27:19 +0900

    Like

--- gomen/src/main/java/com/tuna/gomen/bookMarkandLike/controller/BookmarkController.java
+++ gomen/src/main/java/com/tuna/gomen/bookMarkandLike/controller/BookmarkController.java
@@ -6,6 +6,7 @@ import com.tuna.gomen.bookMarkandLike.entity.Bookmark;
 import com.tuna.gomen.bookMarkandLike.service.BookmarkService;
 import com.tuna.gomen.bookMarkandLike.service.BookmarkService2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,23 +28,33 @@ public class BookmarkController {
         return bookmarkService.getBookmarkedPostsByUserId(userId);
     }
 
+    /// /////////////////////////////////////////////////
+    /// //////////////////////////////////////////
+    /// //////////////////////////////////////////////////
+    /// ///////////////////////////////////////////////////////////////////
     //    아래부터는 command
 
     @Autowired
     private BookmarkService2 bookmarkService2;
 
     @PostMapping("/add")
-//    http://localhost:8080/gomen/bookmarks/add?userId=숫자&postId=숫자
-    public BookmarkDTO addBookmark(@RequestParam Integer userId, @RequestParam Integer postId) {
+// URL: http://localhost:8080/gomen/bookmarks/add
+    public BookmarkDTO addBookmark(@RequestBody Bookmark request) {
+        System.out.println("컨트롤러: Received postId: " + request.getPostId());
 
-        // 디버그용 로그 출력
-        System.out.println("컨트롤러: Received postId: " + postId);
-
-        // postId가 null일 경우 예외 처리
-        if (postId == null) {
+        if (request.getPostId() == null) {
             throw new IllegalArgumentException("Post ID cannot be null");
         }
-        return bookmarkService2.addBookmark(userId, postId);
+
+        return bookmarkService2.addBookmark(request.getUserId(), request.getPostId());
+    }
+
+
+    @DeleteMapping("/remove")
+    //http://localhost:8080/gomen/bookmarks/remove?userId=숫자&postId=숫자(db에 존재하는 데이터 아니면 오류뜸)
+    public ResponseEntity<String> removeBookmark(@RequestParam Integer userId, @RequestParam Integer postId) {
+        bookmarkService2.removeBookmark(userId, postId);
+        return ResponseEntity.ok("Bookmark removed successfully");
     }
 
 }
---
+++ gomen/src/main/java/com/tuna/gomen/bookMarkandLike/dto/LikeDTO.java
@@ -0,0 +1,15 @@
+package com.tuna.gomen.bookMarkandLike.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeDTO {
+    private Integer userId;
+    private Integer postId;
+}
