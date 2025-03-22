dd728fe1a08ea5c2ccf66251b5f9d628f4e29aea
SungMin-hub <sungmin806@gmail.com>
2025-03-22 18:47:15 +0900

    MentoringSpace create commit
    # Conflicts:
    #	gomen/src/main/java/com/tuna/gomen/mapper/MentoringBoardMapper.java

---
+++ gomen/src/main/java/com/tuna/gomen/mentoringspace/command/MentoringSpaceRepository.java
@@ -0,0 +1,6 @@
+package com.tuna.gomen.mentoringspace.command;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentoringSpaceRepository extends JpaRepository<MentoringSpace, Integer> {
+}
