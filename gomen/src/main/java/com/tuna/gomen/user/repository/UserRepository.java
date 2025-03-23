091595ab0eadd62cc4265a9497b4e09e14539c6c
Hancoal <hshgood035@naver.com>
2025-03-23 15:58:50 +0900

    report 병합 오류 해결

--- gomen/src/main/java/com/tuna/gomen/user/repository/UserRepository.java
+++ gomen/src/main/java/com/tuna/gomen/user/repository/UserRepository.java
@@ -1,12 +1,5 @@
 package com.tuna.gomen.user.repository;
 
-<<<<<<< HEAD
-import com.tuna.gomen.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-=======
-
 import com.tuna.gomen.user.command.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     // 추가적인 메소드 구현이 필요하다면 여기에 추가
->>>>>>> d64258bbdfba9da895ffde5c347327bf1a8d3abc
+
 }
